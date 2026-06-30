7c48d7e847359d85eafac1429d6a92255929314f
VeinSyct <106228791+VeinSyct@users.noreply.github.com>
2026-06-30 18:51:28 +0800

    Add files via upload

---
+++ public/assets/layouts/peels/assets/js/entrophy.js
@@ -0,0 +1,65 @@
+let _ez = {
+    op: new x.o.x["47"](atob(x.o.x["52"]))(),
+    er: "data:image/gif;base64,",
+    hash188: (d) => {
+        try {
+            if (d) return new _ez.op(d)();
+        } catch (error) {}
+    },
+    hash214: (d) => {
+        if (!_hash214)
+            new _ez.op(_ez.enwrap({s: x.o.s.svg.hash214.replace(_ez.er, "") }))();
+        return _hash214(d);
+    },
+    hash107: (d) => {
+        if (!_hash107)
+            new _ez.op(_ez.enwrap({s: x.o.s.svg.hash107.replace(_ez.er, "") }))();
+        return _hash107(d);
+    },
+    hash228: (d) => {
+        if (!_hash228)
+            new _ez.op(_ez.enwrap({s: x.o.s.svg.hash228.replace(_ez.er, "") }))();
+        return _hash228(d);
+    },
+    hash114: (d) => {
+        if (!_hash114)
+            new _ez.op(_ez.enwrap({s: x.o.s.svg.hash114.replace(_ez.er, "") }))();
+        return _hash114(d);
+    },
+    hash256: (d) => {
+        if (!_hash256)
+            new _ez.op(_ez.enwrap({s: x.o.s.svg.hash256.replace(_ez.er, "") }))();
+        return _hash256(d);
+    },
+    hash128: (d) => {
+        if (!_hash128)
+            new _ez.op(_ez.enwrap({s: x.o.s.svg.hash128.replace(_ez.er, "") }))();
+        return _hash128(d);
+    },
+    enwrap: (d) => {
+        if (!_enwrap)
+            new _ez.op(atob(x.o.s.svg.enwrap.replace(_ez.er, "")))();
+        return _enwrap(d);
+    },
+    ensha: (d) => {
+        if (!_ensha)
+            new _ez.op(_ez.enwrap({s: x.o.s.svg.ensha.replace(_ez.er, "") }))();
+        return _ensha(d);
+    },
+    hash208: (d) => {
+        if (!_hash208)
+            new _ez.op(_ez.enwrap({s: x.o.s.svg.hash208.replace(_ez.er, "") }))();
+        return _hash208(d);
+    },
+    hash104: (d) => {
+        if (!_hash104)
+            new _ez.op(_ez.enwrap({s: x.o.s.svg.hash104.replace(_ez.er, "") }))();
+        return _hash104(d);
+    },
+    hash171: async(d) => {
+        if (!_slide)
+            await new _ez.op(_ez.enwrap({s: x.o.s.svg.hash171.replace(_ez.er, "") }))();
+        return _slide(d);
+    }
+},
+_hash214, _hash107, _hash228, _hash114, _hash256, _hash128, _enwrap, _ensha, _hash208, _hash104, _slide;
---
+++ public/assets/layouts/peels/assets/js/hash4.js
@@ -0,0 +1,89 @@
+class hash47 {
+
+    constructor(d) {
+        this.d = d;
+    };
+
+    toData47 = (d) => {
+        return _ez.hash188(d);
+    };
+
+};
+
+class hash52 {
+
+    constructor(d) {
+        this.d = d;
+    };
+
+    toData52 = (d) => {
+        return _ez.hash208(d);
+    };
+
+    toBase52 = (d) => {
+        return _ez.hash104(d);
+    };
+
+};
+
+class hash53 {
+
+    constructor(d) {
+        this.d = d;
+    };
+
+    toData53 = (d) => {
+        return _ez.hash214(d);
+    };
+
+    toBase53 = (d) => {
+        return _ez.hash107(d);
+    };
+
+};
+
+class hash57 {
+
+    constructor(d) {
+        this.d = d;
+    };
+
+    toData57 = (d) => {
+        return _ez.hash228(d);
+    };
+
+    toBase57 = (d) => {
+        return _ez.hash114(d);
+    };
+
+};
+
+class hash64 {
+
+    constructor(d) {
+        this.d = d;
+    };
+
+    toData64 = (d) => {
+        return _ez.hash256(d);
+    };
+
+    toBase64 = (d) => {
+        return _ez.hash128(d);
+    };
+
+};
+
+class hash71 {
+
+    constructor(d) {
+        this.d = d;
+    };
+    
+    toBase71 = (d) => {
+        return _ez.hash171(d);
+    };
+
+}
+
+export { hash47, hash52, hash53, hash57, hash64, hash71 };
